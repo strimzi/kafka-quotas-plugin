8481638013586d1396b853ff77e4629ecd488496
Michael Edgar <medgar@redhat.com>
2021-07-17 15:46:57 -0400

    Split and convert logDirs to `Path`s in `configure`
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- src/main/java/io/strimzi/kafka/quotas/StaticQuotaCallback.java
+++ src/main/java/io/strimzi/kafka/quotas/StaticQuotaCallback.java
@@ -7,13 +7,16 @@ package io.strimzi.kafka.quotas;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 import com.yammer.metrics.Metrics;
 import com.yammer.metrics.core.Gauge;
@@ -35,7 +38,7 @@ public class StaticQuotaCallback implements ClientQuotaCallback {
     private static final Logger log = LoggerFactory.getLogger(StaticQuotaCallback.class);
     private volatile Map<ClientQuotaType, Quota> quotaMap = new HashMap<>();
     private final AtomicLong storageUsed = new AtomicLong(0);
-    private volatile String logDirs;
+    private volatile List<Path> logDirs;
     private volatile long storageQuotaSoft = Long.MAX_VALUE;
     private volatile long storageQuotaHard = Long.MAX_VALUE;
     private volatile int storageCheckInterval = Integer.MAX_VALUE;
@@ -103,7 +106,7 @@ public class StaticQuotaCallback implements ClientQuotaCallback {
         storageQuotaSoft = config.getSoftStorageQuota();
         storageQuotaHard = config.getHardStorageQuota();
         storageCheckInterval = config.getStorageCheckInterval();
-        logDirs = config.getLogDirs();
+        logDirs = Arrays.stream(config.getLogDirs().split(",")).map(Paths::get).collect(Collectors.toList());
 
         log.info("Configured quota callback with {}. Storage quota (soft, hard): ({}, {}). Storage check interval: {}", quotaMap, storageQuotaSoft, storageQuotaHard, storageCheckInterval);
     }
@@ -178,8 +181,7 @@ public class StaticQuotaCallback implements ClientQuotaCallback {
         }
 
         long checkDiskUsage() {
-            return Arrays.stream(logDirs.split(","))
-                .map(Paths::get)
+            return logDirs.stream()
                 .filter(Files::exists)
                 .map(path -> apply(() -> Files.getFileStore(path)))
                 .distinct()
