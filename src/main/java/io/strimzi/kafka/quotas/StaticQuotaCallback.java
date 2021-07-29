4aeedf5c361241a3106cb05a3da650be6698a5cd
Stanislav Knot <sknot@redhat.com>
2021-07-29 15:07:55 +0200

    getAndUpdate
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- src/main/java/io/strimzi/kafka/quotas/StaticQuotaCallback.java
+++ src/main/java/io/strimzi/kafka/quotas/StaticQuotaCallback.java
@@ -74,15 +74,20 @@ public class StaticQuotaCallback implements ClientQuotaCallback {
     /**
      * Put a small delay between logging
      */
-    private void maybeLog(AtomicLong latLoggedMessageTimeMs, String format, Object... args) {
-        latLoggedMessageTimeMs.getAndUpdate(current -> {
-            long now = System.currentTimeMillis();
+    private void maybeLog(AtomicLong lastLoggedMessageTimeMs, String format, Object... args) {
+        long now = System.currentTimeMillis();
+        final boolean[] shouldLog = {true};
+        lastLoggedMessageTimeMs.getAndUpdate(current -> {
             if (now - current >= LOGGING_DELAY_MS) {
-                log.debug(format, args);
+                shouldLog[0] = true;
                 return now;
             }
+            shouldLog[0] = false;
             return current;
         });
+        if (shouldLog[0]) {
+            log.debug(format, args);
+        }
     }
 
     @Override
